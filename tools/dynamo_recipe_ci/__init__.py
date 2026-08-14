5bafa0fa03bed31536faec1aafa959815c19e3ee
Harry Lee <harrli@nvidia.com>
2026-08-13 21:12:32 -0700

    Fix Dynamo recipe tool copyright header

--- tools/dynamo_recipe_ci/__init__.py
+++ tools/dynamo_recipe_ci/__init__.py
@@ -1,1 +1,3 @@
 """Dynamo recipe compatibility corpus runner."""
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
